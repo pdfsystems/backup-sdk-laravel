7e9cc7c08b732e63008977e05195c58de0a48f32
Rob Pungello <rob@pdfsystems.com>
2025-09-11 10:58:15 -0400

    Remove default base URL

--- config/pdf-backup.php
+++ config/pdf-backup.php
@@ -2,7 +2,7 @@
 
 // config for Pdfsystems/BackupClient
 return [
-    'base_url' => env('BACKUP_BASE_URL', 'https://backup.pdfsystems.com'),
+    'base_url' => env('BACKUP_BASE_URL'),
     'token' => env('BACKUP_TOKEN'),
     'application_id' => env('BACKUP_APPLICATION_ID'),
 ];
